501c487db549127fba9a0928a6aea41b905a2d85
rohn <rohn@rocana.com>
2017-04-30 15:46:19 -0500

    initial commit

---
+++ content.js
@@ -0,0 +1,38 @@
+function highlightPRStatus() {
+  var pullRequest = [].slice.call(document.querySelectorAll('.js-issue-row'), 0);
+  pullRequest.forEach(function (pr) {
+    var text = pr.querySelector('.opened-by + span > a')
+    if (!!text) {
+      var textContent = text.textContent.trim();
+        var d = pr.querySelector('.opened-by + span > a');
+      if (textContent == 'Changes requested') {
+        d.classList.add('changes-requested');
+        d.classList.remove('muted-link');
+      }
+      else if (textContent == 'Approved') {
+        d.classList.add('approved');
+        d.classList.remove('muted-link');
+      }
+    }
+  });
+}
+
+var observer = new MutationObserver(function(mutations) {
+  var needsRemoval = false;
+  mutations.forEach(function(mutation) {
+    Array.prototype.slice.call(mutation.addedNodes).forEach(function(node) {
+      if (node instanceof Element && (node.querySelector('.js-issue-row') || node.classList.contains('js-issue-row'))) {
+        needsRemoval = true;
+      }
+    });
+  });
+
+  if (needsRemoval) {
+    highlightPRStatus();
+  }
+});
+
+var container = document.getElementById('js-repo-pjax-container');
+observer.observe(container, {childList: true, subtree: true});
+
+highlightPRStatus();
