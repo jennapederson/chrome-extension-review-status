6dd04c9f6016c45f052c61b96e3139071a374d47
Jenna Pederson <jenna@612softwarefoundry.com>
2017-06-06 12:22:32 -0500

    Add partial review approval

--- content.js
+++ content.js
@@ -4,13 +4,18 @@ function highlightPRStatus() {
     var text = pr.querySelector('.opened-by + span > a')
     if (!!text) {
       var textContent = text.textContent.trim();
-        var d = pr.querySelector('.opened-by + span > a');
+      var d = pr.querySelector('.opened-by + span > a');
       if (textContent == 'Changes requested') {
         d.classList.add('changes-requested');
         d.classList.remove('muted-link');
       }
       else if (textContent == 'Approved') {
-        d.classList.add('approved');
+        if (d.getAttribute('aria-label') === '1 review approval') {
+          d.classList.add('partially-approved');
+          text.textContent = 'Partially Approved';
+        } else {
+          d.classList.add('approved');
+        }
         d.classList.remove('muted-link');
       }
     }
